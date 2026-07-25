eb9946810d3bc3553db218ffc859a06f6f49b509
Moises Javier Revi <moisesjr3vi@gmail.com>
2026-07-25 18:57:57 -0400

    feat: add user management routes with role-based authorization

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 import express, { Express } from "express";
-import userRouter from "./modules/users/user.routes";
+import userRouter, { userManagementRouter } from "./modules/users/user.routes";
 import projectRouter from "./modules/projects/project.routes";
 import taskRouter from "./modules/tasks/task.routes";
 
@@ -9,5 +9,6 @@ app.use(express.json());
 app.use('/api/auth', userRouter);
 app.use('/api/projects', projectRouter);
 app.use('/api/tasks', taskRouter);
+app.use('/api/users', userManagementRouter);
 
 export default app;
